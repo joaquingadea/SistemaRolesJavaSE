be3ddc2de7429d80ad5c45f609539cdbd5842e9e
Joaquin Adolfo Gadea <joaquingadea@gmail.com>
2024-07-12 00:22:42 -0300

    hice visible la interfaz grafica del login

--- src/main/java/com/mycompany/sistemaroles/SistemaRoles.java
+++ src/main/java/com/mycompany/sistemaroles/SistemaRoles.java
@@ -1,7 +1,12 @@
 package com.mycompany.sistemaroles;
+
+import com.mycompany.sistemaroles.igu.Login;
+
 public class SistemaRoles {
 
     public static void main(String[] args) {
-        
+        Login pantalla = new Login();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
     }
 }
