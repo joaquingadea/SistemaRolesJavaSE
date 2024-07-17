2af22e399247ae86a9b265e8ce4452bbfcb5ebe5
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2024-07-17 12:56:22 -0300

    ahora al iniciar sesion los usuarios tienen acceso a su pantalla y en caso de no tenes informacion la tabla se muestra un mensaje en pantalla

--- src/main/java/com/mycompany/sistemaroles/igu/Normal.java
+++ src/main/java/com/mycompany/sistemaroles/igu/Normal.java
@@ -1,6 +1,16 @@
 
 package com.mycompany.sistemaroles.igu;
+
+import com.mycompany.sistemaroles.logica.ControladoraLogica;
+import com.mycompany.sistemaroles.logica.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 public class Normal extends javax.swing.JFrame {
+    ControladoraLogica control = new ControladoraLogica();
     public Normal() {
         initComponents();
     }
@@ -15,13 +25,15 @@ public class Normal extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         tblTablaUsuarios.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {},
-                {},
-                {},
-                {}
+
             },
             new String [] {
 
@@ -65,6 +77,48 @@ public class Normal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        List<Usuario> listaUsuarios = new ArrayList<>(); 
+        DefaultTableModel carTabla;
+        carTabla = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int row,int column){
+                return false;
+            }
+        };
+        
+        listaUsuarios = control.traerDatosUs();
+        
+        String titulos[] = {"ID","Nombre","Apellido","Profesion","Departamento"};
+        carTabla.setColumnIdentifiers(titulos);
+        if(listaUsuarios != null){
+            for(Usuario us : listaUsuarios){
+                Object usuario[] = {us.getId(),us.getNombre(),us.getApellido(),us.getProfesion(),us.getDepartamento()};
+                carTabla.addRow(usuario);
+            }
+        }
+        else{
+            mensaje("Lista vacia","error","Lista sin informacion de usuarios");
+        }
+        tblTablaUsuarios.setModel(carTabla);
+    }//GEN-LAST:event_formWindowOpened
+    //metodos
+    public void mensaje(String titulo, String tipoMnj, String mensaje){
+        if(tipoMnj.equals("error")){
+            JOptionPane panel = new JOptionPane(mensaje);
+            panel.setMessageType(JOptionPane.ERROR_MESSAGE);
+            JDialog dialog = panel.createDialog(titulo);
+            dialog.setAlwaysOnTop(true);
+            dialog.setVisible(true);
+        }
+        else if(tipoMnj.equals("info")){
+            JOptionPane info = new JOptionPane(mensaje);
+            info.setMessageType(JOptionPane.INFORMATION_MESSAGE);
+            JDialog dialog = info.createDialog(titulo);
+            dialog.setAlwaysOnTop(true);
+            dialog.setVisible(true);
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
